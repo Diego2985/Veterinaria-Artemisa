f2019bf441133c5d75c6df4c972fd4e9198ba6c8
Matias Alarcon <matiasalarcon55@hotmail.com>
2021-11-02 14:10:43 -0300

    Mejora de codigo

--- src/main/webapp/js/mapa-paseadores.js
+++ src/main/webapp/js/mapa-paseadores.js
@@ -30,17 +30,33 @@ function mostrarPaseadores(latitud, longitud){
         var casa = new H.map.Marker({lat: latitud, lng: longitud}, {icon:pointer});
         map.addObject(casa);
 
-        var paseador = new H.map.Icon('images/paseador.png', {size: {w: 40, h: 40}});
-        var paseador1 = new H.map.Marker({lat:-34.58856, lng:-58.41066}, {icon: paseador});
-        const distance = casa.getGeometry().distance(paseador1.getGeometry());
-        const html1='<div>Paseador</div>' +
-            '<div>Se encuenta a <br />'+Math.round(distance)+' mts.</div>'
-        paseador1.setData(html1);
-        group.addObject(paseador1);
+        const customMarker=(latitud, longitud)=>{
+            const markerPaseador = new H.map.Icon('images/paseador.png', {size: {w: 40, h: 40}});
+            const paseador = new H.map.Marker({lat:latitud, lng:longitud}, {icon: markerPaseador});
+            return paseador;
+        }
 
-        var paseador2 = new H.map.Marker({lat:-34.585991, lng:-58.407848}, {icon: paseador});
+        const htmlPaseador=(inicio, paseador)=> {
+            const distancia = inicio.getGeometry().distance(paseador.getGeometry());
+            return "<div><strong>Paseador</strong></div>" +
+                "<div>5 <i style='color: #FFE445' class=\"fas fa-star\"></i></div>" +
+                "<div>Se encuentra a <strong>" + Math.round(distancia) + " mts.</strong></div>"
+        }
 
-        map.addObject(paseador2);
+        const addBubble=(html, paseador)=>{
+            paseador.setData(html);
+            group.addObject(paseador);
+        }
+
+        const generarData=(inicio, latitud, longitud)=>{
+            const paseador = customMarker(latitud, longitud);
+            const html=htmlPaseador(casa, paseador);
+            addBubble(html, paseador)
+        }
+
+        generarData(casa, -34.58856, -58.41066)
+        generarData(casa, -34.585991, -58.407848)
+        generarData(casa, -34.588322, -58.409309)
 
         var service = platform.getSearchService();
         service.reverseGeocode({

543e51d8e066a674d0380b1724d125c2c572cad2
Matias Alarcon <matiasalarcon55@hotmail.com>
2021-11-02 13:43:30 -0300

    Custom markers y bubbles inicial

--- src/main/webapp/js/mapa-paseadores.js
+++ src/main/webapp/js/mapa-paseadores.js
@@ -14,10 +14,34 @@ function mostrarPaseadores(latitud, longitud){
         var behavior = new H.mapevents.Behavior(new    H.mapevents.MapEvents(map));
         const ui = H.ui.UI.createDefault(map, maptypes, 'es-ES');
 
-        var icon = new H.map.Icon('https://cdn0.iconfinder.com/data/icons/dog-4/100/dog-10-256.png');
-        var marker = new H.map.Marker({lat: latitud, lng: longitud}, {icon: icon});
+        var group = new H.map.Group();
+        map.addObject(group);
 
-        map.addObject(marker);
+        group.addEventListener('tap', function (evt) {
+
+            var bubble = new H.ui.InfoBubble(evt.target.getGeometry(), {
+                content: evt.target.getData()
+            });
+            // show info bubble
+            ui.addBubble(bubble);
+        }, false);
+
+        var pointer = new H.map.Icon('images/home.png', {size: {w: 40, h: 40}});
+        var paseador = new H.map.Icon('images/paseador.png', {size: {w: 40, h: 40}});
+
+        var casa = new H.map.Marker({lat: latitud, lng: longitud}, {icon:pointer});
+        map.addObject(casa);
+
+        var paseador1 = new H.map.Marker({lat:-34.58856, lng:-58.41066}, {icon: paseador});
+        const distance = casa.getGeometry().distance(paseador1.getGeometry());
+        const html1='<div>Paseador</div>' +
+            '<div>Se encuenta a <br />'+Math.round(distance)+' mts.</div>'
+        paseador1.setData(html1);
+        group.addObject(paseador1);
+
+        var paseador2 = new H.map.Marker({lat:-34.585991, lng:-58.407848}, {icon: paseador});
+
+        map.addObject(paseador2);
 
         var service = platform.getSearchService();
         service.reverseGeocode({

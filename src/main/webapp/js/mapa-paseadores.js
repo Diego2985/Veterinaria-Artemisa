538f50012184be54c5950c9e26f653501f894dd4
Matias Alarcon <matiasalarcon55@hotmail.com>
2021-11-02 14:10:50 -0300

    Mejora de codigo

--- src/main/webapp/js/mapa-paseadores.js
+++ src/main/webapp/js/mapa-paseadores.js
@@ -1,17 +1,17 @@
-function mostrarPaseadores(latitud, longitud){
-    window.addEventListener("load",()=>{
+function mostrarPaseadores(latitud, longitud) {
+    window.addEventListener("load", () => {
         var platform = new H.service.Platform({
             'apikey': MAP_KEY
         });
         var maptypes = platform.createDefaultLayers();
 
-        const mapOptions={
+        const mapOptions = {
             zoom: 17,
-            center: { lat: latitud , lng: longitud }
+            center: {lat: latitud, lng: longitud}
         }
-        const mapaElement=document.getElementById("mapContainer");
-        const map=new H.Map(mapaElement,maptypes.vector.normal.map,mapOptions)
-        var behavior = new H.mapevents.Behavior(new    H.mapevents.MapEvents(map));
+        const mapaElement = document.getElementById("mapContainer");
+        const map = new H.Map(mapaElement, maptypes.vector.normal.map, mapOptions)
+        var behavior = new H.mapevents.Behavior(new H.mapevents.MapEvents(map));
         const ui = H.ui.UI.createDefault(map, maptypes, 'es-ES');
 
         var group = new H.map.Group();
@@ -27,30 +27,30 @@ function mostrarPaseadores(latitud, longitud){
         }, false);
 
         var pointer = new H.map.Icon('images/home.png', {size: {w: 40, h: 40}});
-        var casa = new H.map.Marker({lat: latitud, lng: longitud}, {icon:pointer});
+        var casa = new H.map.Marker({lat: latitud, lng: longitud}, {icon: pointer});
         map.addObject(casa);
 
-        const customMarker=(latitud, longitud)=>{
+        const customMarker = (latitud, longitud) => {
             const markerPaseador = new H.map.Icon('images/paseador.png', {size: {w: 40, h: 40}});
-            const paseador = new H.map.Marker({lat:latitud, lng:longitud}, {icon: markerPaseador});
+            const paseador = new H.map.Marker({lat: latitud, lng: longitud}, {icon: markerPaseador});
             return paseador;
         }
 
-        const htmlPaseador=(inicio, paseador)=> {
+        const htmlPaseador = (inicio, paseador) => {
             const distancia = inicio.getGeometry().distance(paseador.getGeometry());
             return "<div><strong>Paseador</strong></div>" +
                 "<div>5 <i style='color: #FFE445' class=\"fas fa-star\"></i></div>" +
                 "<div>Se encuentra a <strong>" + Math.round(distancia) + " mts.</strong></div>"
         }
 
-        const addBubble=(html, paseador)=>{
+        const addBubble = (html, paseador) => {
             paseador.setData(html);
             group.addObject(paseador);
         }
 
-        const generarData=(inicio, latitud, longitud)=>{
+        const generarData = (inicio, latitud, longitud) => {
             const paseador = customMarker(latitud, longitud);
-            const html=htmlPaseador(casa, paseador);
+            const html = htmlPaseador(casa, paseador);
             addBubble(html, paseador)
         }
 
@@ -62,9 +62,9 @@ function mostrarPaseadores(latitud, longitud){
         service.reverseGeocode({
             at: `${mapOptions.center.lat},${mapOptions.center.lng}`
         }, (result) => {
-            const locacion=document.getElementById("locacion")
-            const datos=result.items[0].address.label
-            locacion.innerHTML=datos
+            const locacion = document.getElementById("locacion")
+            const datos = result.items[0].address.label
+            locacion.innerHTML = datos
         });
 
     })

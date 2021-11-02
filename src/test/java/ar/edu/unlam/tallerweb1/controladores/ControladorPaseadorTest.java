b5f000b272c00e2eda2c5c3a601379579a4d457a
Matias Alarcon <matiasalarcon55@hotmail.com>
2021-11-02 18:06:48 -0300

    Se ordenaron los tests y se elimino la lista de paseadores

--- src/test/java/ar/edu/unlam/tallerweb1/controladores/ControladorPaseadorTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/controladores/ControladorPaseadorTest.java
@@ -6,9 +6,6 @@ import ar.edu.unlam.tallerweb1.servicios.ServicioPaseador;
 import org.junit.Test;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -16,8 +13,6 @@ public class ControladorPaseadorTest {
 
     private Double latitud = -34.588902;
     private Double longitud = -58.409851;
-    private String coordenadasString =latitud+","+longitud;
-    private Coordenadas coordenadasObject;
     private ModelAndView mav;
     private Usuario usuario=new Usuario();
     private ServicioPaseador servicioPaseador=mock(ServicioPaseador.class);
@@ -47,22 +42,16 @@ public class ControladorPaseadorTest {
     private void givenUnUsuarioYSuUbicacion() {
         usuario.setEmail("usuario@correo.com");
         usuario.setPassword("12345");
-
-        coordenadasObject=new Coordenadas(coordenadasString, latitud, longitud);
-
-        when(servicioPaseador.obtenerCoordenadasDentroDeUnObjeto(coordenadasString)).thenReturn(coordenadasObject);
     }
 
     private ModelAndView whenEnviaLasCoordenadas() {
-        return controladorPaseador.recibirLasCoordenadasDeUbicacion(coordenadasString);
+        return controladorPaseador.recibirLasCoordenadasDeUbicacion(latitud, longitud);
     }
 
     private void thenDeberiaRecibirlasEnElController(ModelAndView mav) {
-        Coordenadas coordenadasObject= (Coordenadas) mav.getModel().get("coordenadas");
         assertThat(mav.getViewName()).isEqualTo("paseador-mapa");
-        assertThat(coordenadasObject).isInstanceOf(Coordenadas.class);
-        assertThat(coordenadasObject.getLatitud()).isEqualTo(latitud);
-        assertThat(coordenadasObject.getLongitud()).isEqualTo(longitud);
+        assertThat(mav.getModel().get("latitud")).isEqualTo(latitud);
+        assertThat(mav.getModel().get("longitud")).isEqualTo(longitud);
     }
 
     @Test
@@ -98,8 +87,8 @@ public class ControladorPaseadorTest {
         paseador.setId(id);
         paseador.setNombre("Matias");
         paseador.setEstrellas(4);
-        Coordenadas coordenadas=servicioPaseador.obtenerCoordenadasDentroDeUnObjeto("-34.58856,-58.41066");
-        paseador.setCoordenadas(coordenadas);
+        paseador.setLatitud(latitud);
+        paseador.setLongitud(longitud);
 
         when(servicioPaseador.obtenerPaseador(id)).thenReturn(paseador);
 
@@ -108,49 +97,5 @@ public class ControladorPaseadorTest {
 
     private void thenDeboObtenerUnPaseador(ModelAndView mav, Paseador paseador) {
         assertThat(mav.getModel().get("paseador")).isEqualTo(paseador);
-    }
-
-    @Test
-    public void obtenerUnaListaDePaseadores(){
-        List<Paseador> listaObtenida=givenUnaListaDePaseadores();
-        mav=whenSolicitoUnaListaDePaseadores();
-        thenDeboObtenerLosPaseadoresCercanos(mav, listaObtenida);
-    }
-
-    private List<Paseador> givenUnaListaDePaseadores() {
-        String coordenadas1="-34.58856,-58.41066";
-        String coordenadas2=latitud+","+longitud;
-        Paseador paseador1=crearPaseador(1L, coordenadas1);
-        Paseador paseador2=crearPaseador(2L, coordenadas2);
-        coordenadasObject=servicioPaseador.obtenerCoordenadasDentroDeUnObjeto(coordenadas2);
-
-        List<Paseador> paseadores=new ArrayList<>();
-        paseadores.add(paseador1);
-        paseadores.add(paseador2);
-
-        when(servicioPaseador.obtenerListaDePaseadoresCercanos(coordenadasObject)).thenReturn(paseadores);
-
-        return paseadores;
-    }
-
-    private ModelAndView whenSolicitoUnaListaDePaseadores() {
-        return controladorPaseador.recibirLasCoordenadasDeUbicacion(coordenadasString);
-    }
-
-    private void thenDeboObtenerLosPaseadoresCercanos(ModelAndView mav, List<Paseador> listaEsperada) {
-        List<Paseador> listaObtenida= (List<Paseador>) mav.getModel().get("paseadores");
-        assertThat(listaObtenida).isEqualTo(listaEsperada);
-    }
-
-    private Paseador crearPaseador(long idPaseador, String coordenadasPaseador) {
-        Long id=idPaseador;
-        Paseador paseador=new Paseador();
-        paseador.setId(id);
-        paseador.setNombre("Matias");
-        paseador.setEstrellas(4);
-        Coordenadas coordenadas=servicioPaseador.obtenerCoordenadasDentroDeUnObjeto(coordenadasPaseador);
-        paseador.setCoordenadas(coordenadas);
-
-        return paseador;
     }
 }

cb62d3af1c6e7ef3f61fa615c2a64f6be0ff8390
Matias Alarcon <matiasalarcon55@hotmail.com>
2021-11-03 16:28:23 -0300

    Implementacion de clase Paseadores en Serviciopaseadortest, y remove del ultimo paseador

--- src/test/java/ar/edu/unlam/tallerweb1/servicios/ServicioPaseadorTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/servicios/ServicioPaseadorTest.java
@@ -1,5 +1,6 @@
 package ar.edu.unlam.tallerweb1.servicios;
 
+import ar.edu.unlam.tallerweb1.Paseadores;
 import ar.edu.unlam.tallerweb1.modelo.Paseador;
 import ar.edu.unlam.tallerweb1.repositorios.RepositorioPaseador;
 import org.junit.Before;
@@ -14,36 +15,12 @@ import static org.mockito.Mockito.*;
 public class ServicioPaseadorTest {
     private static final Double LATITUD = -34.588902;
     private static final Double LONGITUD = -58.409851;
-    private Paseador paseador1 = new Paseador();
-    private Paseador paseador2 = new Paseador();
-    private Paseador paseador3 = new Paseador();
     private RepositorioPaseador repositorioPaseador=mock(RepositorioPaseador.class);
     private ServicioPaseador servicioPaseador=new ServicioPaseadorImpl(repositorioPaseador);
     private Integer distancia=500;
     private Double diferenciaLatitud;
     private Double diferenciaLongitud;
-    private List<Paseador> listaDePaseadores=new ArrayList<>();
-
-    @Before
-    public void init() {
-        Double latPaseador = -34.58856;
-        Double longPaseador = -58.41066;
-        crearPaseador(paseador1, latPaseador, longPaseador);
-
-        latPaseador = -34.585991;
-        longPaseador = -58.407848;
-        crearPaseador(paseador2, latPaseador, longPaseador);
-
-        latPaseador = -34.58581;
-        longPaseador = -58.41485;
-        crearPaseador(paseador3, latPaseador, longPaseador);
-    }
-
-    private void crearPaseador(Paseador paseador, Double latitud, Double longitud) {
-        paseador.setEstrellas(5);
-        paseador.setLatitud(latitud);
-        paseador.setLongitud(longitud);
-    }
+    private List<Paseador> listaDePaseadores= Paseadores.crearPaseadores();
 
     @Test
     public void solicitarUnaListaDePaseadoresCercanos() {
@@ -53,11 +30,10 @@ public class ServicioPaseadorTest {
     }
 
     private void givenUnaListaDePaseadoresYDiferenciasDePuntosCalculadas() {
-        listaDePaseadores.add(paseador1);
-        listaDePaseadores.add(paseador2);
-
         diferenciaLatitud=servicioPaseador.calcularPuntosDeDiferencia(LATITUD, distancia);
         diferenciaLongitud=servicioPaseador.calcularPuntosDeDiferencia(LONGITUD, distancia);
+
+        listaDePaseadores.remove(2);
 
         when(repositorioPaseador.obtenerPaseadoresCercanos(LATITUD, LONGITUD, diferenciaLatitud, diferenciaLongitud)).thenReturn(listaDePaseadores);
     }
@@ -68,8 +44,7 @@ public class ServicioPaseadorTest {
 
     private void thenDebeRetornarLosPaseadoresCercanos(List<Paseador> paseadoresObtenidos) {
         assertThat(paseadoresObtenidos).hasSize(2);
-        assertThat(paseadoresObtenidos).contains(paseador1);
-        assertThat(paseadoresObtenidos).contains(paseador2);
-        assertThat(paseadoresObtenidos).doesNotContain(paseador3);
+        assertThat(paseadoresObtenidos).contains(listaDePaseadores.get(0));
+        assertThat(paseadoresObtenidos).contains(listaDePaseadores.get(1));
     }
 }

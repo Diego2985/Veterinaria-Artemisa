abd4f1e731420bb2c35be948e3ffb02978f8a98f
Matias Alarcon <matiasalarcon55@hotmail.com>
2021-11-03 12:12:19 -0300

    Test para traer a los paseadores cercanos

--- src/test/java/ar/edu/unlam/tallerweb1/controladores/ControladorPaseadorTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/controladores/ControladorPaseadorTest.java
@@ -6,6 +6,9 @@ import ar.edu.unlam.tallerweb1.servicios.ServicioPaseador;
 import org.junit.Test;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.Mockito.*;
 
@@ -13,6 +16,7 @@ public class ControladorPaseadorTest {
 
     private Double latitud = -34.588902;
     private Double longitud = -58.409851;
+    private Integer distancia=500;
     private ModelAndView mav;
     private Usuario usuario=new Usuario();
     private ServicioPaseador servicioPaseador=mock(ServicioPaseador.class);
@@ -45,7 +49,7 @@ public class ControladorPaseadorTest {
     }
 
     private ModelAndView whenEnviaLasCoordenadas() {
-        return controladorPaseador.recibirLasCoordenadasDeUbicacion(latitud, longitud);
+        return controladorPaseador.obtenerPaseadoresCercanosA500mts(latitud, longitud);
     }
 
     private void thenDeberiaRecibirlasEnElController(ModelAndView mav) {
@@ -97,5 +101,48 @@ public class ControladorPaseadorTest {
 
     private void thenDeboObtenerUnPaseador(ModelAndView mav, Paseador paseador) {
         assertThat(mav.getModel().get("paseador")).isEqualTo(paseador);
+    }
+
+    @Test
+    public void obtenerPaseadoresCercanos(){
+        List<Paseador> paseadoresEsperados=givenUnaListaDePaseadores();
+        mav=whenSolicitoLosPaseadores();
+        thenDeboObtenerLosPaseadoresCercanos(paseadoresEsperados, mav);
+    }
+
+    private List<Paseador> givenUnaListaDePaseadores() {
+        Double latPaseador = -34.58856;
+        Double longPaseador = -58.41066;
+        Paseador paseador1 = crearPaseador(latPaseador, longPaseador);
+
+        latPaseador = -34.585991;
+        longPaseador = -58.407848;
+        Paseador paseador2 = crearPaseador(latPaseador, longPaseador);
+
+        List<Paseador> paseadores=new ArrayList<>();
+        paseadores.add(paseador1);
+        paseadores.add(paseador2);
+
+        when(servicioPaseador.obtenerListaDePaseadoresCercanos(latitud, longitud, distancia)).thenReturn(paseadores);
+
+        return paseadores;
+    }
+
+    private ModelAndView whenSolicitoLosPaseadores() {
+        return controladorPaseador.obtenerPaseadoresCercanosA500mts(latitud, longitud);
+    }
+
+    private void thenDeboObtenerLosPaseadoresCercanos(List<Paseador> paseadoresEsperados, ModelAndView mav) {
+        List<Paseador> obtenidos= (List<Paseador>) mav.getModel().get("paseadores");
+        assertThat(obtenidos).hasSize(2);
+        assertThat(obtenidos).isEqualTo(paseadoresEsperados);
+    }
+
+    private Paseador crearPaseador(Double latitud, Double longitud) {
+        Paseador paseador=new Paseador();
+        paseador.setEstrellas(5);
+        paseador.setLatitud(latitud);
+        paseador.setLongitud(longitud);
+        return paseador;
     }
 }

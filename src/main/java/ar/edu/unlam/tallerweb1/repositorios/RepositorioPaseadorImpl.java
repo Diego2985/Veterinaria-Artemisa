5a6d4a6a20f92feb82207ebea26fb0c84c6c600e
Matias Alarcon <matiasalarcon55@hotmail.com>
2021-11-03 12:12:02 -0300

    Agregado de transactional a repository

--- src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioPaseadorImpl.java
+++ src/main/java/ar/edu/unlam/tallerweb1/repositorios/RepositorioPaseadorImpl.java
@@ -6,10 +6,12 @@ import org.hibernate.SessionFactory;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
+@Transactional
 public class RepositorioPaseadorImpl implements RepositorioPaseador{
     private SessionFactory sessionFactory;
     @Autowired

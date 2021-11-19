245143b783f7c92fc076513547862e189795914f
Matias Alarcon <matiasalarcon55@hotmail.com>
2021-11-19 17:17:23 -0300

    Test de mascotas que sean perros

--- src/test/java/ar/edu/unlam/tallerweb1/controladores/ControladorPaseadorMascotaTest.java
+++ src/test/java/ar/edu/unlam/tallerweb1/controladores/ControladorPaseadorMascotaTest.java
@@ -1,7 +1,9 @@
 package ar.edu.unlam.tallerweb1.controladores;
 
 import ar.edu.unlam.tallerweb1.Paseadores;
+import ar.edu.unlam.tallerweb1.modelo.Mascota;
 import ar.edu.unlam.tallerweb1.modelo.Paseador;
+import ar.edu.unlam.tallerweb1.servicios.ServicioMascotas;
 import ar.edu.unlam.tallerweb1.servicios.ServicioPaseador;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,9 +19,10 @@ import static org.assertj.core.api.Assertions.*;
 
 public class ControladorPaseadorMascotaTest {
     private List<Paseador> paseadores = Paseadores.crearPaseadores();
-    private Integer distancia;
+    private Integer distancia = 500;
     private ServicioPaseador servicioPaseador = mock(ServicioPaseador.class);
-    private ControladorPaseador controladorPaseador = new ControladorPaseador(servicioPaseador);
+    private ServicioMascotas servicioMascotas = mock(ServicioMascotas.class);
+    private ControladorPaseador controladorPaseador = new ControladorPaseador(servicioPaseador, servicioMascotas);
     private final Double LATITUD = -34.588902;
     private final Double LONGITUD = -58.409851;
     private ModelAndView mav;
@@ -40,7 +43,6 @@ public class ControladorPaseadorMascotaTest {
     }
 
     private void givenUnaDistanciaYUnaListaDePaseadoresCercanos() {
-        distancia = 500;
         ArrayList<Paseador> paseadoresCercanos = new ArrayList<>();
         paseadoresCercanos.add(paseadores.get(0));
         paseadoresCercanos.add(paseadores.get(1));
@@ -57,5 +59,41 @@ public class ControladorPaseadorMascotaTest {
         assertThat(obtenidos).hasSize(2);
     }
 
+    @Test
+    public void verificarQueLaMascotaElegidaSeaUnPerro() {
+        givenUnUsuarioConSusMascotas();
+        mav = whenSolicitoLasMascotas();
+        thenDeberiaObtenerLosQueSonPerros(mav);
+    }
 
+    private void givenUnUsuarioConSusMascotas() {
+        Mascota perro = new Mascota();
+        perro.setId(1L);
+        perro.setTipo("Perro");
+        perro.setNombre("Firulais");
+
+        Mascota gato = new Mascota();
+        gato.setId(2L);
+        gato.setTipo("Gato");
+        gato.setNombre("Garfield");
+
+        List<Mascota> perros = new ArrayList<>();
+        perros.add(perro);
+
+        when(servicioMascotas.obtenerPerrosPorIdUsuario(1L)).thenReturn(perros);
+    }
+
+    private ModelAndView whenSolicitoLasMascotas() {
+        return controladorPaseador.obtenerPaseadoresCercanos(LATITUD, LONGITUD, distancia, request);
+    }
+
+    private void thenDeberiaObtenerLosQueSonPerros(ModelAndView mav) {
+        List<Mascota> obtenidas = (List<Mascota>) mav.getModel().get("perros");
+        assertThat(obtenidas).hasSize(1);
+    }
+
+    @Test
+    public void recibirTodosLosPaseosActivosDelUsuario() {
+
+    }
 }
